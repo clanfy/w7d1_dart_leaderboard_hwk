6590146019792e625b580b83e0afc6d27afe6a65
Clacson <cookiellacson@gmail.com>
2016-11-07 16:13:17 +0000

    get set name and get set nickname created, tests passed

--- app/src/main/java/com/example/user/dartleaderboard/Player.java
+++ app/src/main/java/com/example/user/dartleaderboard/Player.java
@@ -24,5 +24,12 @@ public class Player {
         return this.nickname;
     }
 
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setNickname(String nickname){
+        this.nickname = nickname;
+    }
 
 }
--- app/src/test/java/com/example/user/dartleaderboard/PlayerTest.java
+++ app/src/test/java/com/example/user/dartleaderboard/PlayerTest.java
@@ -24,6 +24,18 @@ public class PlayerTest {
             assertEquals("No Fear Lanfear", player1.getNickname());
         }
 
+        @Test
+        public void testCanSetName(){
+            player1.setName("Poopie Lanfear");
+            assertEquals("Poopie Lanfear", player1.getName());
+        }
+
+        @Test
+        public void testCanSetNickname(){
+            player1.setNickname("No Pants Lanfear");
+            assertEquals("No Pants Lanfear", player1.getNickname());
+        }
+
     }
 
 
